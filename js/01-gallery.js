ee3ca76aca455fba740b05502e8451f7eeaba0e1
Anton <anton_surkov@rambler.ru>
2022-06-16 20:33:45 +0300

    complete tast-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.innerHTML = addGalleryItemMurkup(galleryItems);
+galleryRef.addEventListener("click", onGalleryItemClick);
+
+function addGalleryItemMurkup(gallery) {
+  return gallery
+    .map((item) => {
+      return `<div class="gallery__item">
+          <a class="gallery__link" href="${item.original}">
+            <img
+              class="gallery__image"
+              src="${item.preview}"
+              data-source="${item.original}"
+              alt="${item.description}"
+            />
+          </a>
+          </div>`;
+    })
+    .join("");
+}
+
+function onGalleryItemClick(e) {
+  e.preventDefault();
+
+  const imgRef = e.target.nodeName === "IMG";
+
+  if (!imgRef) {
+    return;
+  } else {
+    const largeItem = e.target.dataset.source;
+
+    showLargeItem(largeItem);
+  }
+}
+
+function showLargeItem(src) {
+  const fullSizeGalleryItem = basicLightbox.create(
+    `
+    <img src = "${src}" width = "1200" height = "800"">
+    `
+  );
+
+  fullSizeGalleryItem.show();
+}
