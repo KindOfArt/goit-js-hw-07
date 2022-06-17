14185184f3aa9f50db781a150afa1079010b1936
Anton <anton_surkov@rambler.ru>
2022-06-17 04:40:55 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,7 +6,7 @@ console.log(galleryItems);
 const galleryRef = document.querySelector(".gallery");
 
 galleryRef.innerHTML = addGalleryItemMurkup(galleryItems);
-galleryRef.addEventListener("click", onGalleryItemClick);
+galleryRef.addEventListener("click", onGalleryImageClick);
 
 function addGalleryItemMurkup(gallery) {
   return gallery
@@ -25,25 +25,40 @@ function addGalleryItemMurkup(gallery) {
     .join("");
 }
 
-function onGalleryItemClick(e) {
+function onGalleryImageClick(e) {
   e.preventDefault();
 
-  const imgRef = e.target.nodeName === "IMG";
+  const IMG_NODE = e.target.nodeName === "IMG";
 
-  if (!imgRef) {
+  if (!IMG_NODE) {
     return;
   } else {
-    const largeItem = e.target.dataset.source;
+    const originalImage = e.target.dataset.source;
+    const popUpImage = createPopUpImageMurkup(originalImage);
 
-    showLargeItem(largeItem);
+    popUpImage.show();
   }
 }
 
-function showLargeItem(src) {
-  const fullSizeGalleryItem = basicLightbox.create(
-    `<img src = "${src}" width = "1280" height = "800"">`,
-    { closable: false }
+function createPopUpImageMurkup(src) {
+  const popUp = basicLightbox.create(
+    `<img src="${src}" width="1280" height="800">`,
+    {
+      onShow: (popUp) => {
+        window.addEventListener("keydown", onEscClick);
+      },
+      onClose: (popUp) => {
+        window.removeEventListener("keydown", onEscClick);
+      },
+    }
   );
+  function onEscClick(e) {
+    if (e.code === "Escape") {
+      popUp.close();
+    } else {
+      return;
+    }
+  }
 
-  fullSizeGalleryItem.show();
+  return popUp;
 }
